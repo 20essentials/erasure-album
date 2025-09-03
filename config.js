bc4daa2bdcbbc4142fd23262aa91522244c24610
DominicDev <s0l0estudi4r@gmail.com>
2025-09-03 17:12:12 +0200

    v0.0.2

---
+++ config.js
@@ -0,0 +1,23 @@
+export const nameBand = ' ERASURE';
+export const nameSongs = [
+	"A LITTLE RESPECT", /*1*/
+	"ALWAYS", /*2*/
+	"OH L’AMOUR!", /*3*/
+	"LOVE TO HATE YOU ", /*4*/
+	"STOP", /*5*/
+	"CHAINS OF LOVE", /*6*/
+	"TAKE A CHANCE ON ME", /*7*/
+	"SOMETIMES", /*8*/
+	"BLUE SAVANNAH", /*9*/
+	"SHIP OF FOOLS", /*10*/
+	"DRAMA", /*11*/
+	"DRAMA (ACT2)", /*12*/
+	"BREATH OF LIFE ", /*13*/
+	"YOU SURROUND ME ", /*14*/
+	"VICTIM OF LOVE", /*15*/
+	"WHO NEEDS LOVE LIKE THAT", /*16*/
+	"BREATHE", /*17*/
+	"RUN TO THE SUN", /*18*/
+	"FINGERS & THUMBS", /*19*/
+	"STAR", /*20*/
+]
